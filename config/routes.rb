de60eeef5d907551d1c3a40c074e10d42ca2465f
pestrada <paul_e_mtz@hotmail.com>
2012-09-27 10:42:10 -0500

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rsmarto::Application.routes.draw do
+  
+  get "home/index"
+
+  root to: 'home#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
