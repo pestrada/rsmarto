4c3355f2885a0947688ec9f4f55d3b978eadf57a
pestrada <paul_e_mtz@hotmail.com>
2012-09-27 10:37:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rsmarto::Application.config.secret_token = 'a1e4b273ce67e7e2f1978b3bb3684da215008c44e124e90bed5b5abc6a02004f30c03bcd645fd7e0f2a1edbc9884973148d45963368d75c478c2f7333f6624e5'
