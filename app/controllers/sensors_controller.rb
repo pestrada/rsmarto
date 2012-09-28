59b19f253edfc4b9ae1dda7aea91091b730a6ee8
pestrada <paul_e_mtz@hotmail.com>
2012-09-28 09:25:39 -0500

    added sensors#create and related views

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -27,4 +27,19 @@ class SensorsController < ApplicationController
     end
   end
   
+  def create
+    @sensor = Sensor.new(params[:sensor])
+    
+    respond_to do |format|
+      if @sensor.save
+        format.html { redirect_to @sensor, notice: 'Sensor was successfully created.' }
+        format.json { render json: @sensor, status: :created, location: @sensor }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @sensor.errors, status: :unprocessable_entity }
+      end
+    end
+    
+  end
+  
 end
