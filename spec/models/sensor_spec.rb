9eddf67c4d733d0a138a58a19ce12b6227ae55ed
pestrada <paul_e_mtz@hotmail.com>
2012-10-04 00:00:18 -0500

    starting to use RSpec

--- spec/models/sensor_spec.rb
+++ spec/models/sensor_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe Sensor do
-  it 'is named Toby' do
-    sensor = Sensor.new
-    sensor.name = 'John'
-    sensor.name.should == 'Toby'
+  
+  subject(:sensor) { Sensor.new }
+  
+  it 'should has a valid name' do
+    sensor.name = 'sensy'
+    sensor.name.should_not be_blank
   end
+  
+  it 'should has a valid measurement' do
+    sensor.measurement = '%'
+    sensor.measurement.should_not be_blank
+  end
+  
 end
